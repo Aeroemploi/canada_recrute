1f85e5fa76351a09dd4ece0aa40d9db4b25d1188
simon <simon.provencher.lepage@gmail.com>
2018-09-07 16:55:52 -0400

    adding language modification.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -140,6 +140,4 @@
 @section('footer_scripts')
     <!-- Bootstrap -->
     <script type="text/javascript" src="{{ asset('assets/js/frontend/bootstrap.min.js') }}"></script>
-
-    {{ $_SERVER['HTTP_HOST'] }}
 @stop
